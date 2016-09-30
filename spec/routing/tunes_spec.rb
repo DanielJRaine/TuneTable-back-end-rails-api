0e166cb0ba2a876469e04ea00d75daed88df61e9
DanielJRaine <ley.danielj@gmail.com>
2016-09-30 08:40:09 -0400

    Write passing test for route /tunes to tunes#index

--- spec/routing/tunes_spec.rb
+++ spec/routing/tunes_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'routes for tunes' do
   it 'routes POST /games to the tunes#create action' do
     expect(post('/tunes')).to route_to('tunes#create')
   end
+
+  it 'routes GET /tunes to the tunes#index action' do
+    expect(get('/tunes')).to route_to('tunes#index')
+  end
 end
