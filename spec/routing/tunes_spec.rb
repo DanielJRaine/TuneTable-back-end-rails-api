22c0343b36ee65600b1aa7c66d828cb38ba4a0d0
DanielJRaine <ley.danielj@gmail.com>
2016-09-30 09:02:48 -0400

    Write test for route tunes/:id to show action

--- spec/routing/tunes_spec.rb
+++ spec/routing/tunes_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe 'routes for tunes' do
   it 'routes GET /tunes to the tunes#index action' do
     expect(get('/tunes')).to route_to('tunes#index')
   end
+
+  it 'routes GET /tunes/:id to the tunes#show action' do
+    expect(get('/tunes/1')).to route_to(
+      controller: 'tunes',
+      action: 'show',
+      id: '1'
+    )
+  end
 end
