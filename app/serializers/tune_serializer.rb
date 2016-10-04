cc4a8daa9af518db4ca88a03b135ef4188d447c9
DanielJRaine <ley.danielj@gmail.com>
2016-10-03 21:03:09 -0400

    Add everything to tunes Serializer

--- app/serializers/tune_serializer.rb
+++ app/serializers/tune_serializer.rb
@@ -1,5 +1,5 @@
 class TuneSerializer < ActiveModel::Serializer
-  attributes :id, :ABCnotation, :tuneTitleT, :editable
+  attributes :id, :ABCnotation, :user_id, :tuneTitleT, :composerC, :originO, :areaA, :meterM, :unitNoteLengthL, :tempoQ, :partsP, :transcriptionZ, :notesN, :groupG, :historyH, :keyK, :rhythmR, :editable
   
   def editable
     scope == object.user
